f7c824ada703a6bfe3d9618fc9844cd3ad2f6b3f
KaloyanBG <39673294+KaloyanBG@users.noreply.github.com>
2018-05-27 18:36:28 +0300

    Add files via upload
    This is a better version, but it needs a little touch.

--- Terracraft/game.js
+++ Terracraft/game.js
@@ -1,11 +1,10 @@
     // Creating variables
 var x8=200,y8=500
 var x=-960,y=0;
+var x2=930;
 var x1=200,y1=658;
 var b=new Image();
 b.src='b.png';
-var s=new Image();
-s.src='sky.png';
 var g=new Image();
 g.src='g.jpg';
 var d=new Image();
@@ -19,14 +18,22 @@ function update() {
         x=x-4;
         x1=x1-4;
     }
-    
+    if(x<=-6600){
+        x=-960;
+        x1=200;
+    }
+    /*if(x<=-6600){
+        x=-960;
+        x1=200;
+    }*/
    
     
 };
 function draw() {
-    context.drawImage(s,x,y ,1890,1080);
+    context.fillStyle="lightblue"
+    context.fillRect(x,y ,7560,1080);
     context.drawImage(b,x8,y8 ,100,165);
-    for (let i=0; i<14; ++i){
+    for (let i=0; i<56; ++i){
         context.drawImage(g,x1+55+(i-9)*135,y1 ,135,135);
         context.drawImage(d,x1+55+(i-9)*135,y1+135 ,135,135);
         context.drawImage(d,x1+55+(i-9)*135,y1+270 ,135,135);
